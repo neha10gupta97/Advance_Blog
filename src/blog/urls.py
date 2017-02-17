94a2b941a67a1c247a33243fa8587e204fb039b3
Neha <neha10gupta97@gmail.com>
2017-02-17 16:32:25 +0530

    blog

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 #from posts import views
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     url(r'^posts/',include("posts.urls",namespace="posts")),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
